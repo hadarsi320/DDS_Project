eca8905fcc60b008b8a6395bb1347277a016a94c
liran <liranhal@campus.techniom.ac.il>
2020-07-01 15:32:30 +0300

    pre-pull

--- vertex.py
+++ vertex.py
@@ -1,7 +1,12 @@
 import socket
+from threading import Thread
 
 
 def vertex(ID):
+    input_file_name = 'input_vertex_' + str(ID) + '.txt'
+    input_file = open(input_file_name, 'r')
+
+    data = input_file.read()
     pass
 
 
