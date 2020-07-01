d3197939218814068b0261ed5ad74901ed7a12b8
liran <liranhal@campus.techniom.ac.il>
2020-07-01 17:08:18 +0300

    pre-pull

--- vertex.py
+++ vertex.py
@@ -1,16 +1,31 @@
 import socket
 from threading import Thread
+from math import log2, ceil
+
+INPUT_DIR = 'input/'
 
 
 def vertex(ID):
-    input_file_name = 'input_vertex_' + str(ID) + '.txt'
-    input_file = open(input_file_name, 'r')
 
-    data = input_file.read()
+    input_file_name = 'input_vertex_' + str(ID) + '.txt'
+    input_file = open(INPUT_DIR + input_file_name, 'r')
+
+    graph_size = input_file.readline()
+    master_port = input_file.readline()
+    master_ip = input_file.readline()
+    udp_listen_port = input_file.readline()
+    tcp_listen_port = input_file.readline()
+
+    master_conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    master_address = (master_port)
+
+
     pass
 
 
 def main():
+
+    vertex('0001')
     pass
 
 
