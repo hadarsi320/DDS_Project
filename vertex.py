9e0916195181d0ee0207bfac0ca4f4c3552c83e8
Hadar <hadarsi@campus.technion.ac.il>
2020-07-02 15:28:48 +0300

    Fixed bug, 99% works

--- vertex.py
+++ vertex.py
@@ -11,7 +11,6 @@ OUTPUT_DIR = 'output/'
 def vertex(ID):
     next_msg = f'next_{ID}'.encode()
     done_msg = f'done_{ID}'.encode()
-
     input_file_name = 'input_vertex_' + str(ID) + '.txt'
     with open(INPUT_DIR + input_file_name, 'r') as input_file:
         data = input_file.read().split('\n')
@@ -78,18 +77,8 @@ def vertex(ID):
             master_send_socket.sendto(next_msg, master)
 
         else:
-            # Send color to children
-            send_color_threads = [Thread(target=send_color,
-                                         args=(child_socket, color, child[1], ID, OUTPUT_DIR, output_file_lock))
-                                  for child, child_socket in zip(children, children_sockets)]
-            for thread in send_color_threads:
-                thread.start()
-
-            # Get color from parent
-            parent_color = None
-            if parent:
-                # TODO message len
-                parent_color = parent_connection[0].recv(4096).decode()
+            parent_color = communicate_colors(ID, color, output_file_lock, children, children_sockets,
+                                              parent, parent_connection[0])
 
             if len(color) > 3:
                 # TreeColoring 8
@@ -99,10 +88,10 @@ def vertex(ID):
                 # TreeColoring 3
                 if shift_down:
                     children_color = color
-                    color = parent_color if parent else (color + 1) % 3
+                    color = parent_color if parent else str((color + 1) % 3)
                     shift_down = False
                 else:
-                    if color == x:
+                    if dec(color) == x:
                         color = min_non_conflicting_color(children_color, parent_color)
                     if x == 3:
                         done_flag = True
@@ -111,15 +100,36 @@ def vertex(ID):
                         x -= 1
 
                 if done_flag:
+                    color = str(dec(color))
                     master_send_socket.sendto(done_msg, master)
                 else:
                     master_send_socket.sendto(next_msg, master)
 
     # TODO check correction of coloring
-    write_color(ID, color)
+    parent_color = communicate_colors(ID, color, output_file_lock, children, children_sockets,
+                                      parent, parent_connection[0])
+    if parent_color == color or int(color) > 2:
+        print(f'ID:{ID}, color: {color}, parent_color: {parent_color}\n')
 
+    write_color(ID, color)
     for close_socket in socket_list:
         close_socket.close()
+
+
+def communicate_colors(ID, color, output_file_lock, children, children_sockets, parent=None, parent_socket=None):
+    # Send color to children
+    send_color_threads = [Thread(target=send_color,
+                                 args=(child_socket, color, child[1], ID, OUTPUT_DIR, output_file_lock))
+                          for child, child_socket in zip(children, children_sockets)]
+    for thread in send_color_threads:
+        thread.start()
+
+    # Get color from parent
+    parent_color = None
+    if parent:
+        # TODO message len
+        parent_color = parent_socket.recv(4096).decode()
+    return parent_color
 
 
 def send_color(tcp_socket: socket, color: str, port: int, ID: str, output_dir: str, file_lock: Lock):
@@ -132,7 +142,7 @@ def send_color(tcp_socket: socket, color: str, port: int, ID: str, output_dir: s
 
 def write_color(ID: str, color: str):
     with open(f'color_vertex_{ID}.txt', 'w') as color_file:
-        color_file.write(str(int(color, 2)))
+        color_file.write(color)
 
 
 def recolor(color, parent_color=None):
@@ -166,9 +176,14 @@ def accept_connection(tcp_socket: socket, socket_list: list):
 
 def min_non_conflicting_color(children_color, parent_color):
     for i in range(3):
-        if children_color != i and parent_color != i:
-            return i
+        if dec(children_color) != i and dec(parent_color) != i:
+            return bin(i)[2:].zfill(2)
+
+
+def dec(binary_string: str):
+    return int(binary_string, 2)
 
 
 if __name__ == '__main__':
-    vertex('00101')
+    print(min_non_conflicting_color(2, 0))
+    # vertex('00101')
